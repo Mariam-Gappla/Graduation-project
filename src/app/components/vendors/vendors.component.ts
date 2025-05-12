83228bd8378858222816f917d41604edd22fb7c5
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2025-05-13 01:44:59 +0300

    add package

--- src/app/components/vendors/vendors.component.ts
+++ src/app/components/vendors/vendors.component.ts
@@ -1,9 +1,9 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-vendors',
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './vendors.component.html',
   styleUrl: './vendors.component.css'
 })
@@ -15,7 +15,7 @@ cards = [
     experience: 2,
     clients: 15,
     likes: 50,
-    bestVendor:0
+    bestVendor:1
   },
   {
     image: 'photo.jpg',
