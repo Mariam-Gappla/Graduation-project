5a034a969b8798e7199f8ad14f68a87aa3657f07
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2025-05-14 19:13:49 +0300

    some edits+vendors page

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,11 +1,51 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  pricingPackages = [
+  {
+    title: "Half day session + Limited photos",
+    price: "3500 EGP"
+  },
+  {
+    title: "Half day session + Unlimited photos",
+    price: "4200 EGP"
+  },
+  {
+    title: "Full day session + Limited photos",
+    price: "5000 EGP"
+  },
+  {
+    title: "Full day session + Unlimited photos",
+    price: "6000 EGP"
+  }
+];
+reviews = [
+  {
+    name: 'Sarah Ali',
+    date: 'March 2025',
+    comment: 'Amazing work! Very professional and creative.',
+    image: 'user1.jpg'
+  },
+  {
+    name: 'Ahmed Mostafa',
+    date: 'January 2025',
+    comment: 'Highly recommend. Made the whole session fun!',
+    image: 'user2.jpg'
+  },
+  {
+    name: 'Laila Kamal',
+    date: 'December 2024',
+    comment: 'Great experience and beautiful photos!',
+    image: 'user3.jpg'
+  }
+];
+
 
 }
