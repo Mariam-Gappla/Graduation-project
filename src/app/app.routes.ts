6973f4e14c962b417b8a2ae8db4ad577312aa475
Mariam-Gappla <119626394+Mariam-Gappla@users.noreply.github.com>
2025-05-18 16:10:20 +0200

    make edit service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 export const routes: Routes = [
  {path:"home",loadComponent:()=>import('../app/components/home/home.component').then(m => m.HomeComponent)},
  {path:"login",loadComponent:()=>import('../app/components/login/login.component').then(m=>m.LoginComponent)},
-//  {path:"",loadComponent:()=>import('../app/components/signup/signup.component').then(m=>m.SignupComponent)},
+ {path:"",loadComponent:()=>import('../app/components/signup/signup.component').then(m=>m.SignupComponent)},
  {path:"vendors/:category",loadComponent:()=>import('../app/components/vendors/vendors.component').then(m=>m.VendorsComponent)},
  {path:"b-details",loadComponent:()=>import('../app/components/b-details/b-details.component').then(b=>b.BDetailsComponent)},
  {path:"booking",loadComponent:()=>import('../app/components/booking/booking.component').then(b=>b.BookingComponent)},
@@ -13,6 +13,7 @@ export const routes: Routes = [
  {path:"editservice",loadComponent:()=>import('../app/components/edit-service/edit-service.component').then(e=>e.EditServiceComponent)},
  {path:"editservice/:id",loadComponent:()=>import('../app/components/edit-service/edit-service.component').then(e=>e.EditServiceComponent)},
  {path:"forgetpassword",loadComponent:()=>import('../app/components/forget-password/forget-password.component').then(m=>m.ForgetPasswordComponent)},
- {path:"",loadComponent:()=>import('../app/AdminComponents/admin-component/admin-component.component').then(m=>m.AdminComponentComponent)},
+ {path:"admin",loadComponent:()=>import('../app/AdminComponents/admin-component/admin-component.component').then(m=>m.AdminComponentComponent)},
+ {path:"vendorservices/:id",loadComponent:()=>import('../app/components/vendorservices/vendorservices.component').then(m=>m.VendorservicesComponent)},
  {path:"**",loadComponent:()=>import('../app/components/error/error.component').then(m=>m.ErrorComponent)},
 ];
